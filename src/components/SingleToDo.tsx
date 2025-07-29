9545f0f9c1f1d56682831183cac3ad65e6618947
CeNan11 <johnrenanlabay75@gmail.com>
2025-07-29 18:14:15 +0800

    Hover nimation added

--- src/components/SingleToDo.tsx
+++ src/components/SingleToDo.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import type {ToDo} from '../model';
+import { FaEdit } from "react-icons/fa";
+import { FaCheck } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa";
 
-type Props ={
+type Props = {
     todos: ToDo;
     toDos: ToDo[];
     setToDos: React.Dispatch<React.SetStateAction<ToDo[]>>
@@ -9,8 +12,19 @@ type Props ={
 
 const SingleToDo: React.FC<Props>= ({todos, toDos, setToDos}) => {
   return (
-    <form>
-        <span>{todos.todo}</span>
+    <form className="flex flex-wrap justify-between px-5 py-6 mt-2 transition-transform duration-300 bg-yellow-500 rounded-md w-[300px] h-[100px] hover:scale-105">
+        <span className="pr-16 break-words text-md font-md">{todos.todo}</span>
+        <div className="flex self-end gap-3 ml-20">
+            <button className="transition-transform duration-300 hover:scale-125">
+                <FaCheck/>
+            </button>
+            <button className="transition-transform duration-300 hover:scale-125">
+                <FaEdit />
+            </button>
+            <button className="transition-transform duration-300 hover:scale-125">
+                <FaTrash />
+            </button>
+        </div>
     </form>
   )
 }
